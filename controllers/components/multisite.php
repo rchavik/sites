9b91e7117cd75a7c8ccb44943d742fdc8f1f3cfd
Rachman Chavik <rchavik@xintesa.com>
2011-04-21 12:10:23 +0700

    override croogo's cache

--- controllers/components/multisite.php
+++ controllers/components/multisite.php
@@ -39,9 +39,37 @@ class MultisiteComponent extends Object {
 
 	}
 
+	/** _setupCache
+	 *  Differentiate croogo's cache prefix so that sites have their own cache
+	 *  List of cache names are from croogo_bootstrap.php
+	 */
+	function _setupCache(&$controller) {
+		$configured = Cache::configured();
+		$croogoCacheNames = array(
+			// components
+			'croogo_blocks', 'croogo_menus', 'croogo_nodes',
+			'croogo_types', 'croogo_vocabularies',
+			// controllers
+			'croogo_vocabularies', 'nodes_promoted', 'nodes_term',
+			'nodes_index', 'contacts_view',
+			);
+		$siteTitle = Inflector::slug(strtolower(Configure::read('Site.title')));
+		for ($i = 0, $ii = count($configured); $i < $ii; $i++) {
+			if (!in_array($configured[$i], $croogoCacheNames)) { continue; }
+			$cacheName = $configured[$i];
+			$setting = Cache::settings($cacheName);
+			$setting = Set::merge($setting, array(
+				'prefix' => 'cake_' . $siteTitle . '_',
+				)
+			);
+			Cache::config($cacheName, $setting);
+		}
+	}
+
 	function startup(&$controller) {
 		$this->_setLookupFields($controller);
 		$this->_setupNodeRelationships($controller);
+		$this->_setupCache($controller);
 	}
 
 	function initialize(&$controller, $settings = array()) {
