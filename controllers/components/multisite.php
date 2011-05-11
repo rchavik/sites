39a1e26da8808ef14c733035de434923d15c4850
Rachman Chavik <rchavik@xintesa.com>
2011-05-11 15:01:40 +0700

    move _setupNodeRelationships to behavior + remove hardcode table/model name

--- controllers/components/multisite.php
+++ controllers/components/multisite.php
@@ -8,37 +8,6 @@ class MultisiteComponent extends Object {
 		$controller->set('sites', ClassRegistry::init('Sites.Site')->find('list'));
 	}
 
-	function _setupNodeRelationships(&$controller) {
-/*
-		$controller->Node->bindModel( array(
-			'hasMany' => array(
-				'Publisher' => array(
-					'className' => 'Sites.SitesNode',
-					'alias' => 'SitesNode',
-					'foreignKey' => 'node_id',
-					),
-				),
-			),
-			false
-		);
-*/
-
-		$controller->Node->bindModel( array(
-			'hasAndBelongsToMany' => array(
-				'Site' => array(
-					'className' => 'Sites.Site',
-					'joinTable' => 'sites_nodes',
-					'foreignKey' => 'node_id',
-					'associationForeignKey' => 'site_id',
-					'unique' => true,
-					),
-				),
-			),
-			false
-		);
-
-	}
-
 	/** _setupCache
 	 *  Differentiate croogo's cache prefix so that sites have their own cache
 	 *  List of cache names are from croogo_bootstrap.php
@@ -68,13 +37,12 @@ class MultisiteComponent extends Object {
 
 	function startup(&$controller) {
 		$this->_setLookupFields($controller);
-		$this->_setupNodeRelationships($controller);
 		$this->_setupCache($controller);
 	}
 
 	function initialize(&$controller, $settings = array()) {
 		if (1 == $controller->Auth->user('role_id') && isset($controller->params['admin'])) {
-			$controller->Node->Behaviors->detach('Sites.SiteFilter');
+			$controller->{$controller->modelClass}->Behaviors->detach('Sites.SiteFilter');
 		}
 		$site = Sites::currentSite();
 	}
--- models/behaviors/site_filter.php
+++ models/behaviors/site_filter.php
@@ -2,20 +2,63 @@
 
 class SiteFilterBehavior extends ModelBehavior {
 
+	function setup(&$model, $config = array()) {
+		$this->_setupNodeRelationships($model, $config);
+	}
+
+	function _setupNodeRelationships(&$model, $config = array()) {
+/*
+		$model->bindModel( array(
+			'hasMany' => array(
+				'Publisher' => array(
+					'className' => 'Sites.SitesNode',
+					'alias' => 'SitesNode',
+					'foreignKey' => 'node_id',
+					),
+				),
+			),
+			false
+		);
+*/
+
+		$joinTable = 'sites_' . Inflector::underscore($model->name);
+		$foreignKey = Inflector::underscore($model->name) . '_id';
+		$with = 'Sites.Sites' . $model->name;
+
+		$model->bindModel( array(
+			'hasAndBelongsToMany' => array(
+				'Site' => array(
+					'className' => 'Sites.Site',
+					'joinTable' => $joinTable,
+					'foreignKey' => $foreignKey,
+					'associationForeignKey' => 'site_id',
+					'unique' => true,
+					'with' => $with,
+					),
+				),
+			),
+			false
+		);
+
+	}
+
 	function beforeFind(&$model, $query) {
 		$site = Sites::currentSite();
+		$joinModel = 'Sites' . $model->name;
+		$dbConfig= ConnectionManager::getInstance()->config->{$model->Site->{$joinModel}->useDbConfig};
+		$foreignKey = Inflector::underscore($model->name) . '_id';
 		$joins = array(
 			array(
 				'type' => 'LEFT',
-				'table' => 'sites_nodes',
-				'alias' => 'SitesNode',
+				'table' => $dbConfig['database'] . '.' . $model->Site->{$joinModel}->useTable,
+				'alias' => $joinModel,
 				'conditions' => array(
-					'Node.id = SitesNode.node_id',
+					$model->name . '.id = ' . $joinModel . '.' . $foreignKey,
 					),
 				),
 			);
 		$query['joins'] = $joins;
-		$query['conditions']['SitesNode.site_id'] = array(Sites::ALL_SITES, $site['Site']['id']);
+		$query['conditions'][$joinModel . '.site_id'] = array(Sites::ALL_SITES, $site['Site']['id']);
 		return $query;
 	}
 
