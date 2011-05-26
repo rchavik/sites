fdaffcf6fe02a9aea00bb6fe8af0e8f6970715e9
Rachman Chavik <rchavik@xintesa.com>
2011-05-26 14:52:44 +0700

    Model site filtering

--- models/behaviors/site_filter.php
+++ models/behaviors/site_filter.php
@@ -3,62 +3,92 @@
 class SiteFilterBehavior extends ModelBehavior {
 
 	function setup(&$model, $config = array()) {
-		$this->_setupNodeRelationships($model, $config);
+		if (empty($config)) {
+			// when $config is empty, we assume that it was loaded by
+			// Croogo hookHelper()
+			$config = array(
+				'relationship' => array(
+					'hasAndBelongsToMany' => array(
+						'Site' => array(
+							'className' => 'Sites.Site',
+							'with' => 'Sites.SitesNode',
+							)
+						)
+					)
+				);
+		}
+		$this->settings[$model->alias] = $config;
+		$this->_setupRelationships($model, $config);
 	}
 
-	function _setupNodeRelationships(&$model, $config = array()) {
-/*
-		$model->bindModel( array(
-			'hasMany' => array(
-				'Publisher' => array(
-					'className' => 'Sites.SitesNode',
-					'alias' => 'SitesNode',
-					'foreignKey' => 'node_id',
-					),
-				),
-			),
-			false
-		);
-*/
-
-		$joinTable = 'sites_' . Inflector::underscore($model->name);
-		$foreignKey = Inflector::underscore($model->name) . '_id';
-		$with = 'Sites.Sites' . $model->name;
-
-		$model->bindModel( array(
-			'hasAndBelongsToMany' => array(
-				'Site' => array(
-					'className' => 'Sites.Site',
-					'joinTable' => $joinTable,
-					'foreignKey' => $foreignKey,
-					'associationForeignKey' => 'site_id',
-					'unique' => true,
-					'with' => $with,
-					),
-				),
-			),
-			false
-		);
-
+	function _setupRelationships(&$model, $config = array()) {
+		if (!empty($this->settings[$model->alias])) {
+			if (!empty($this->settings[$model->alias]['relationship'])) {
+				$model->bindModel($config['relationship'], false);
+			}
+		}
 	}
 
 	function beforeFind(&$model, $query) {
 		$site = Sites::currentSite();
-		$joinModel = 'Sites' . $model->name;
-		$dbConfig= ConnectionManager::getInstance()->config->{$model->Site->{$joinModel}->useDbConfig};
-		$foreignKey = Inflector::underscore($model->name) . '_id';
-		$joins = array(
-			array(
-				'type' => 'LEFT',
-				'table' => $dbConfig['database'] . '.' . $model->Site->{$joinModel}->useTable,
-				'alias' => $joinModel,
-				'conditions' => array(
-					$model->name . '.id = ' . $joinModel . '.' . $foreignKey,
-					),
-				),
+		$sites = array(Sites::ALL_SITES, $site['Site']['id']);
+
+		$setting = Set::merge(
+			array('relationship' => array(), 'joins' => array()),
+			$this->settings[$model->alias]
 			);
+		extract($setting);
+
+		if (!empty($joins)) {
+			foreach ($joins as $join => &$joinConfig) {
+				if (empty($joinConfig)) continue;
+
+				// link to Site model
+				$foreignKey = $joinConfig['alias']. '.site_id';
+				if (empty($joinConfig['conditions'][$foreignKey])) {
+					$joinConfig['conditions'][$foreignKey] = $sites;
+				}
+
+				$foreignKey = $joinConfig['alias']. '.' . Inflector::underscore($model->alias) . '_id';
+				$condition = "{$model->alias}.{$model->primaryKey} = {$foreignKey}";
+				if (!in_array($condition, $joinConfig['conditions'])) {
+					$joinConfig['conditions'][] = $condition;
+				}
+			}
+		}
+
+		if (!empty($relationship)) {
+			$relation = key($relationship);
+			$foreignKey = $model->{$relation}['Site']['foreignKey'];
+			switch ($relation) {
+
+			case 'belongsTo':
+				$query['conditions'][$model->alias . '.' . $foreignKey] = $sites;
+				break;
+
+			case 'hasAndBelongsToMany':
+			default:
+				$with = $model->{$relation}['Site']['with'];
+				$joinModel = $model->{$with};
+				$ds = $joinModel->getDataSource();
+				$associationForeignKey = $model->{$relation}['Site']['associationForeignKey'];
+
+				$joins = Set::merge($joins, array(
+					array(
+						'type' => 'LEFT',
+						'table' => $ds->fullTableName($joinModel),
+						'alias' => $joinModel->alias,
+						'conditions' => array(
+							"{$model->alias}.{$model->primaryKey} = {$joinModel->alias}.$foreignKey",
+							),
+						),
+					));
+				$query['conditions'][$joinModel->alias . '.' . $associationForeignKey] = $sites;
+				break;
+			}
+		}
+
 		$query['joins'] = $joins;
-		$query['conditions'][$joinModel . '.site_id'] = array(Sites::ALL_SITES, $site['Site']['id']);
 		return $query;
 	}
 
