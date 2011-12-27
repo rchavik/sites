31fa8db7d58f89f2525794961fe7f94589f48c31
Rachman Chavik <rchavik@xintesa.com>
2011-12-27 14:03:42 +0700

    update plugin.json

--- Config/manifest.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$pluginManifest = array(
-	'name' => 'Sites',
-	'description' => 'Croogo Multisite Plugin',
-	'author' => 'Rachman Chavik - Xintesa',
-	'authorEmail' => 'contact@xintesa.com',
-	);
