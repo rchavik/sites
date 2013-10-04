860fa167481e62bfba9a8db4f583226c9299a2c7
Pedro Fernandes Steimbruch <pedrofsteimbruch@gmail.com>
2013-10-04 01:28:51 -0300

    SiteFilterBehavior fixed
    There was a trying to access a non existent index due to a empty
    sites table when it tries to get the currentSite. I've fixed it with a
    simple check on the currentSite method returning.

--- Model/Behavior/SiteFilterBehavior.php
+++ Model/Behavior/SiteFilterBehavior.php
@@ -36,7 +36,12 @@ class SiteFilterBehavior extends ModelBehavior {
 		}
 		$this->_setupRelationships($model, $this->settings[$model->alias]);
 		$site = Sites::currentSite();
-		$sites = array_unique(array(Sites::ALL_SITES, $site['Site']['id']));
+
+		$sites = array(Sites::ALL_SITES);
+
+		if ($site) {
+			array_unique(array(Sites::ALL_SITES, $site['Site']['id']));
+		}
 
 		$setting = Set::merge(
 			array('relationship' => array(), 'joins' => array()),
